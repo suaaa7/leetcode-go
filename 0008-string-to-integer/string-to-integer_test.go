438528f77df1abcd1f0b18920e7c96444f7e1105
suaaa7 <spuaaa7@gmail.com>
2019-08-15 19:48:18 +0900

    Add 0008

---
+++ 0008-string-to-integer/string-to-integer_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	tests := []string{
+		"0",
+		"42",
+		"     -42",
+		"4193 with words",
+		"words and 987",
+		"-91283472332",
+	}
+	expecteds := []int{
+		0,
+		42,
+		-42,
+		4193,
+		0,
+		-2147483648,
+	}
+	for i := 0; i < len(tests); i++ {
+		actual := myAtoi(tests[i])
+		fmt.Printf("nums = %v\n", tests[i])
+		if actual != expecteds[i] {
+			t.Fatalf("actual -> %v expected -> %v\n", actual, expecteds[i])
+		}
+	}
+}
