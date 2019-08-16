37ec50882ae0fa829051a37e79cba015e31910b8
suaaa7 <spuaaa7@gmail.com>
2019-08-16 16:28:00 +0900

    Add 0020

---
+++ 0020-valid-parentheses/valid-parentheses.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import (
+	"strings"
+)
+
+func isValid(s string) bool {
+	for strings.Contains(s, "()") || strings.Contains(s, "{}") || strings.Contains(s, "[]"){
+		s = strings.Replace(s, "()", "", -1)
+		s = strings.Replace(s, "{}", "", -1)
+		s = strings.Replace(s, "[]", "", -1)
+	}
+
+	return s == ""
+}
---
+++ 0020-valid-parentheses/valid-parentheses_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsValid(t *testing.T) {
+	tests := []string{
+		"()",
+		"(){}[]",
+		"(]",
+		"([)]",
+		"{[]}",
+		"([]",
+	}
+	expecteds := []bool{
+		true,
+		true,
+		false,
+		false,
+		true,
+		false,
+	}
+	for i := 0; i < len(tests); i++ {
+		actual := isValid(tests[i])
+		fmt.Printf("nums = %v\n", tests[i])
+		if actual != expecteds[i] {
+			t.Fatalf("actual -> %v expected -> %v\n", actual, expecteds[i])
+		}
+	}
+}
