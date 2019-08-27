4628a2bca0b82f6607fa7e02c4effa303447123c
suaaa7 <spuaaa7@gmail.com>
2019-08-28 00:31:12 +0900

    Update 0198 test

--- 0198-house-robber/house-robber_test.go
+++ 0198-house-robber/house-robber_test.go
@@ -10,11 +10,13 @@ func TestRob(t *testing.T) {
 		[]int{1, 2, 3, 1},
 		[]int{2, 7, 9, 3, 1},
 		[]int{2, 1, 1, 2},
+		[]int{2, 7, 9, 3, 1, 20},
 	}
 	expecteds := []int{
 		4,
 		12,
 		4,
+		31,
 	}
 	for i := 0; i < len(tests); i++ {
 		actual := rob(tests[i])
