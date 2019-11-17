e0f248d2b1dc17c84c5bba3ab1f0fda544233d61
suaaa7 <spuaaa7@gmail.com>
2019-11-18 01:47:09 +0900

    Update tool/listnode_test

--- tool/listnode_test.go
+++ tool/listnode_test.go
@@ -37,6 +37,37 @@ func TestInts2List(t *testing.T) {
 	}
 }
 
+func TestInts2ListWithCycle(t *testing.T) {
+	tests := [][]int{
+		[]int{1, 2},
+		[]int{1, 2, 3},
+	}
+	expecteds := []*ListNode{
+		&ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+			},
+		},
+		&ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 3,
+				},
+			},
+		},
+	}
+	for i := range expecteds {
+		actual := Ints2ListWithCycle(tests[i], -1)
+		fmt.Printf("test = %v\n", tests[i])
+		if !reflect.DeepEqual(actual, expecteds[i]) {
+			t.Fatalf("actual -> %v expected -> %v\n", actual, expecteds[i])
+		}
+	}
+}
+
 func TestList2Ints(t *testing.T) {
 	tests := []*ListNode{
 		&ListNode{
