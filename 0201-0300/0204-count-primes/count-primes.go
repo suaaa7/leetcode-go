d40d10305cf8c3705d611768bb9f0d633d8a1a80
suaaa7 <spuaaa7@gmail.com>
2019-11-19 23:36:12 +0900

    Update 0204

--- 0201-0300/0204-count-primes/count-primes.go
+++ 0201-0300/0204-count-primes/count-primes.go
@@ -1,34 +1,21 @@
 package leetcode
 
 func countPrimes(n int) int {
-	if n < 3 {
-		return 0
-	}
-
-	if n == 3 {
-		return 1
-	}
-
-	var primes []int
-	primes = append(primes, 2)
-	primes = append(primes, 3)
-
-	for i := 3; i < n; i += 2 {
-		if i%3 == 0 {
+	isComposite := make([]bool, n)
+	for i := 2; i*i < n; i++ {
+		if isComposite[i] {
 			continue
 		}
-
-		isPrime := true
-		for j := 0; isPrime && j < len(primes); j++ {
-			if i%primes[j] == 0 {
-				isPrime = false
-			}
+		for j := i * i; j < n; j = j + i {
+			isComposite[j] = true
 		}
-
-		if isPrime {
-			primes = append(primes, i)
+	}
+	var count int
+	for i := 2; i < n; i++ {
+		if !isComposite[i] {
+			count++
 		}
 	}
 
-	return len(primes)
+	return count
 }
