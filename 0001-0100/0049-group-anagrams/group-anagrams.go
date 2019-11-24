4904e6610acbf36b4e9326903c909331a21ae34e
suaaa7 <spuaaa7@gmail.com>
2019-11-24 23:29:41 +0900

    Update 0049

--- 0001-0100/0049-group-anagrams/group-anagrams.go
+++ 0001-0100/0049-group-anagrams/group-anagrams.go
@@ -1,11 +1,35 @@
 package leetcode
 
+import (
+	"sort"
+)
+
+type sortRunes []rune
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
 func groupAnagrams(strs []string) [][]string {
-	if len(strs) == 0 {
-		return [][]string{}
+	tmp := make(map[string][]string, len(strs))
+	for _, str := range strs {
+		runes := []rune(str)
+		sort.Sort(sortRunes(runes))
+		tmp[string(runes)] = append(tmp[string(runes)], str)
 	}
 
-	var result [][]string
+	result := make([][]string, 0, len(tmp))
+	for _, v := range tmp {
+		result = append(result, v)
+	}
 
 	return result
 }
--- 0001-0100/0049-group-anagrams/group-anagrams_test.go
+++ 0001-0100/0049-group-anagrams/group-anagrams_test.go
@@ -3,20 +3,40 @@ package leetcode
 import (
 	"fmt"
 	"reflect"
+	"sort"
 	"testing"
 )
 
+type sortStringsSlice [][]string
+
+func (s sortStringsSlice) Len() int {
+	return len(s)
+}
+
+func (s sortStringsSlice) Less(i, j int) bool {
+	return len(s[i]) > len(s[j])
+}
+
+func (s sortStringsSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
 func TestGroupAnagrams(t *testing.T) {
 	tests := [][]string{
-		[]string{},
+		[]string{"eat", "tea", "tan", "ate", "nat", "bat"},
 		[]string{},
 	}
 	expecteds := [][][]string{
-		[][]string{},
+		[][]string{
+			[]string{"eat", "tea", "ate"},
+			[]string{"tan", "nat"},
+			[]string{"bat"},
+		},
 		[][]string{},
 	}
 	for i := 0; i < len(tests); i++ {
 		actual := groupAnagrams(tests[i])
+		sort.Sort(sortStringsSlice(actual))
 		fmt.Printf("test = %v\n", tests[i])
 		if !reflect.DeepEqual(actual, expecteds[i]) {
 			t.Fatalf("actual -> %v expected -> %v\n", actual, expecteds[i])
